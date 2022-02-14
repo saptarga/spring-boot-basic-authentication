65a301b8d6618bbc5a5f6b595a35dc1f35cc08fd
saptarga <saptarga@gmail.com>
2022-02-14 13:50:15 +0700

    Test pipeline

--- src/main/java/com/example/Demo/Basic/Authentication/endpoint/IUserEndpoint.java
+++ src/main/java/com/example/Demo/Basic/Authentication/endpoint/IUserEndpoint.java
@@ -5,6 +5,7 @@ import com.example.Demo.Basic.Authentication.entity.User;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
